7dba83c57f647642dc7f642dc4f6f6def8008334
Mattias Wadman <wader@spotify.com>
2020-07-13 20:54:39 +0200

    wip

--- internal/format/id3v2/id3v2.go
+++ internal/format/id3v2/id3v2.go
@@ -1,5 +1,9 @@
 package id3v2
 
+// https://id3.org/id3v2.3.0
+// https://id3.org/id3v2.4.0-structure
+// https://id3.org/id3v2.4.0-frames
+
 import (
 	"fmt"
 	"fq/internal/decode"
@@ -25,6 +29,56 @@ func (d *TagDecoder) SyncSafeU32() uint64 {
 		((u & 0x007f0000) >> 2) |
 		((u & 0x00007f00) >> 1) |
 		((u & 0x0000007f) >> 0))
+}
+
+func (d *TagDecoder) DecodeFrame(version int) uint64 {
+	var id string
+	switch version {
+	case 2:
+		id = d.UTF8(3)
+		d.SeekRel(-3 * 8)
+	case 3, 4:
+		id = d.UTF8(4)
+		d.SeekRel(-4 * 8)
+	}
+
+	d.FieldNoneFn(id, func() {
+
+		switch version {
+		case 2:
+			// Frame ID   "XXX"
+			// Frame size $xx xx xx
+		case 3:
+			// Frame ID   $xx xx xx xx  (four characters)
+			// Size       $xx xx xx xx
+			// Flags      $xx xx
+		case 4:
+			// Frame ID      $xx xx xx xx  (four characters)
+			// Size      4 * %0xxxxxxx  (synchsafe integer)
+			// Flags         $xx xx
+		}
+	})
+
+	// TODO
+	return 0
+}
+
+func (d *TagDecoder) DecodeFrames(version int, size uint64) {
+
+	for size > 0 {
+		// TODO: what was this about?
+		var padding uint64
+		for d.U8() == 0 {
+			padding++
+		}
+		if padding > 0 {
+			d.SeekRel(-int64(padding) * 8)
+			d.FieldBytesLen("padding", padding)
+		}
+
+		size -= d.DecodeFrame(version)
+	}
+
 }
 
 // Decode ID3v2
