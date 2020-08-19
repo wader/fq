6bba1b0f212e04f9cb299735cd690138b3f3cc57
Mattias Wadman <mattias.wadman@gmail.com>
2020-08-20 00:27:52 +0200

    readseeker

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/pkg/profile v1.5.0
-	golang.org/x/tools v0.0.0-20200815165600-90abf76919f3 // indirect
+	golang.org/x/tools v0.0.0-20200819193742-d088b475e336 // indirect
 	golang.org/x/tools/gopls v0.4.4 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	honnef.co/go/tools v0.0.1-2020.1.5 // indirect
--- pkg/output/text/text.go
+++ pkg/output/text/text.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 )
 
+const lineBytes = 16
+
 var FieldOutput = &decode.FieldOutput{
 	Name: "text",
 	New:  func(f *decode.Field) decode.FieldWriter { return &FieldWriter{f: f} },
@@ -23,35 +25,42 @@ func (o *FieldWriter) output(cw *columnwriter.Writer, f *decode.Field, depth int
 	startBit := f.Decoder.AbsPos(f.Range.Start)
 	stopBit := f.Decoder.AbsPos(f.Range.Stop)
 
-	startByte := startBit / 8
-	stopByte := stopBit / 8
-	if startBit%8 != 0 {
-		stopByte++
+	if startBit != stopBit {
+		stopBit--
 	}
 
-	lineAddrByte := int((startByte >> 4) << 4)
-	lineAddrByteOffset := int(startByte & 0xf)
+	startByte := startBit / 8
+	stopByte := stopBit / 8
 
+	startLineByte := int((startByte / lineBytes) * lineBytes)
+	startLineByteOffset := int(startByte % lineBytes)
+	stopLineByte := int((stopByte / lineBytes) * lineBytes)
 	addrLines := 1
+
+	// log.Printf("startBit: %x\n", startBit)
+	// log.Printf("stopBit: %x\n", stopBit)
+
+	// log.Printf("startByte: %x\n", startByte)
+	// log.Printf("stopByte: %x\n", stopByte)
+
+	// log.Printf("startLineByte: %x\n", startLineByte)
+	// log.Printf("stopLineByte: %x\n", stopLineByte)
+
+	// log.Printf("addrLines: %x\n", addrLines)
 
 	if len(f.Children) == 0 {
 		b, err := f.BitBuf().BytesBitRange(0, f.Range.Length(), 0)
 		if err != nil {
 			return err
 		}
+		addrLines = ((stopLineByte - startLineByte) / lineBytes) + 1
 
-		addrStopBytes := lineAddrByteOffset + int(stopByte) - int(startByte)
-		addrLines = addrStopBytes / 16
-		if addrStopBytes%16 != 0 {
-			addrLines++
-		}
-
-		fmt.Fprintf(cw.Columns[2], "%s", hexdump.Hexpairs(b, 16, lineAddrByteOffset))
-		fmt.Fprintf(cw.Columns[4], "%s", hexdump.Printable(b, lineAddrByteOffset))
+		fmt.Fprintf(cw.Columns[2], "%s", hexdump.Hexpairs(b, lineBytes, startLineByteOffset))
+		fmt.Fprintf(cw.Columns[4], "%s", hexdump.Printable(b, startLineByteOffset))
 	}
 
 	for i := 0; i < addrLines; i++ {
-		fmt.Fprintf(cw.Columns[0], "%.8x\n", int(lineAddrByte)+i*16)
+		fmt.Fprintf(cw.Columns[0], "%.8x\n", int(startLineByte)+i*lineBytes)
 		fmt.Fprintf(cw.Columns[1], "\n")
 		fmt.Fprintf(cw.Columns[3], "|\n")
 		fmt.Fprintf(cw.Columns[5], "|\n")
@@ -82,7 +91,7 @@ func (o *FieldWriter) output(cw *columnwriter.Writer, f *decode.Field, depth int
 }
 
 func (o *FieldWriter) Write(w io.Writer) error {
-	cw := columnwriter.New(w, []int{8, 1, 47, 1, 16, 1, -1})
+	cw := columnwriter.New(w, []int{8, 1, lineBytes*3 - 1, 1, lineBytes, 1, -1})
 	cw.Columns[1].Wrap = true
 	cw.Columns[2].Wrap = true
 	cw.Columns[4].Wrap = true
