514bd809866f9c5919c513bcc719183a8dcaf346
Mattias Wadman <mattias.wadman@gmail.com>
2020-11-05 10:10:16 +0100

    wip

--- pkg/decode/registry.go
+++ pkg/decode/registry.go
@@ -22,10 +22,9 @@ func NewRegistry() *Registry {
 func (r *Registry) register(groupName string, format *Format, single bool) *Format {
 	formats, ok := r.allGroups[groupName]
 	if ok {
-		// TODO: we want to overload
-		// if !single {
-		// 	panic(fmt.Sprintf("%s: already registered", groupName))
-		// }
+		if !single {
+			panic(fmt.Sprintf("%s: already registered", groupName))
+		}
 	} else {
 		formats = []*Format{}
 	}
