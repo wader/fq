085d08624bc6dbac46a5c91fcaa12499db4b5c31
Mattias Wadman <mattias.wadman@gmail.com>
2020-11-06 17:20:51 +0100

    wip

--- pkg/format/icc/icc.go
+++ pkg/format/icc/icc.go
@@ -14,8 +14,43 @@ func init() {
 	})
 }
 
-func iccDecode(d *decode.D) interface{} {
+func xyzType(d *decode.D) {
+	d.FieldUTF8("type", 4)
+	d.FieldU32("reserved")
+	d.FieldFP32("X")
+	d.FieldFP32("Y")
+	d.FieldFP32("Z")
+}
 
+var signatureToDecode = map[string]func(d *decode.D){
+	"wtpt": xyzType,
+	"bkpt": xyzType,
+	"rXYZ": xyzType,
+	"gXYZ": xyzType,
+	"bXYZ": xyzType,
+}
+
+func iccDecode(d *decode.D) interface{} {
+	/*
+	   0..3 Profile size uInt32Number
+	   4..7 CMM Type signature see below
+	   8..11 Profile version number see below
+	   12..15 Profile/Device Class signature see below
+	   16..19 Color space of data (possibly a derived space) [i.e. “the canonical input space”] see below
+	   20..23 Profile Connection Space (PCS) [i.e. “the canonical output space”] see below
+	   24..35 Date and time this profile was first created dateTimeNumber
+	   36..39 ‘acsp’ (61637370h) profile file signature
+	   40..43 Primary Platform signature see below
+	   44..47 Flags to indicate various options for the CMM such as distributed processing and caching options see below
+	   48..51 Device manufacturer of the device for which this profile is created see below
+	   52..55 Device model of the device for which this profile is created see below
+	   56..63 Device attributes unique to the particular device setup such as media type see below
+	   64..67 Rendering Intent see below
+	   68..79 The XYZ values of the illuminant of the Profile Connection Space. This must correspond to D50. It is explained in more detail in A.1. XYZNumber
+	   80..83 Profile Creator signature see below
+	   84..99 Profile ID see below
+	   100..127 28 bytes reserved for future expansion - must be set to zeros
+	*/
 	d.FieldStructFn("header", func(d *decode.D) {
 		d.FieldU32("size")
 		d.FieldUTF8("cmm_type_signature", 4)
@@ -50,35 +85,19 @@ func iccDecode(d *decode.D) interface{} {
 		d.FieldArrayFn("table", func(d *decode.D) {
 			for i := uint64(0); i < tagCount; i++ {
 				d.FieldStructFn("element", func(d *decode.D) {
-					d.FieldUTF8("signature", 4)
+					signature := d.FieldUTF8("signature", 4)
 					offset := d.FieldU32("offset")
 					size := d.FieldU32("size")
-					d.FieldBitBufRange("data", int64(offset)*8, int64(size)*8)
+
+					if fn, ok := signatureToDecode[signature]; ok {
+						d.SubRangeFn(int64(offset)*8, int64(size)*8, func() { fn(d) })
+					} else {
+						d.FieldBitBufRange("data", int64(offset)*8, int64(size)*8)
+					}
 				})
 			}
 		})
 	})
-
-	/*
-	   0..3 Profile size uInt32Number
-	   4..7 CMM Type signature see below
-	   8..11 Profile version number see below
-	   12..15 Profile/Device Class signature see below
-	   16..19 Color space of data (possibly a derived space) [i.e. “the canonical input space”] see below
-	   20..23 Profile Connection Space (PCS) [i.e. “the canonical output space”] see below
-	   24..35 Date and time this profile was first created dateTimeNumber
-	   36..39 ‘acsp’ (61637370h) profile file signature
-	   40..43 Primary Platform signature see below
-	   44..47 Flags to indicate various options for the CMM such as distributed processing and caching options see below
-	   48..51 Device manufacturer of the device for which this profile is created see below
-	   52..55 Device model of the device for which this profile is created see below
-	   56..63 Device attributes unique to the particular device setup such as media type see below
-	   64..67 Rendering Intent see below
-	   68..79 The XYZ values of the illuminant of the Profile Connection Space. This must correspond to D50. It is explained in more detail in A.1. XYZNumber
-	   80..83 Profile Creator signature see below
-	   84..99 Profile ID see below
-	   100..127 28 bytes reserved for future expansion - must be set to zeros
-	*/
 
 	return nil
 }
