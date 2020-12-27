4ae2e9828cf207eb088059d0a34d54b7f51099b8
Mattias Wadman <mattias.wadman@gmail.com>
2020-12-27 15:19:41 +0100

    wip

--- pkg/bitio/buffer.go
+++ pkg/bitio/buffer.go
@@ -28,6 +28,61 @@ const (
 	LittleEndian
 )
 
+const progressPrecision = 1024
+
+type progressReaderSeeker2 struct {
+	rs             io.ReadSeeker
+	pos            int64
+	length         int64
+	partitions     []bool
+	partitionsRead int64
+	progressFn     func(readBytes int64, length int64)
+}
+
+func newProgressReaderSeeker2(rs io.ReadSeeker, length int64, fn func(pos int64, length int64)) *progressReaderSeeker2 {
+	return &progressReaderSeeker2{
+		rs:         rs,
+		length:     length,
+		partitions: make([]bool, progressPrecision+1),
+		progressFn: fn,
+	}
+}
+
+func (prs *progressReaderSeeker2) Read(p []byte) (n int, err error) {
+	n, err = prs.rs.Read(p)
+	newPos := prs.pos + int64(n)
+	lastPartitionsRead := prs.partitionsRead
+	partitionSize := prs.length / (progressPrecision - 1)
+
+	partStart := prs.pos / partitionSize
+	partEnd := newPos / partitionSize
+	for i := partStart; i <= partEnd; i++ {
+		if prs.partitions[i] {
+			continue
+		}
+		prs.partitions[i] = true
+		prs.partitionsRead++
+	}
+
+	if lastPartitionsRead != prs.partitionsRead {
+		readBytes := partitionSize * prs.partitionsRead
+		if readBytes > prs.length {
+			readBytes = prs.length
+		}
+		prs.progressFn(readBytes, prs.length)
+	}
+
+	prs.pos = newPos
+
+	return n, err
+}
+
+func (prs *progressReaderSeeker2) Seek(offset int64, whence int) (int64, error) {
+	pos, err := prs.rs.Seek(offset, whence)
+	prs.pos = pos
+	return pos, err
+}
+
 type progressReaderSeeker struct {
 	RS         io.ReadSeeker
 	Length     int64
@@ -79,8 +134,12 @@ func NewBufferFromReadSeeker(rs io.ReadSeeker) (*Buffer, error) {
 
 	// // TODO: move
 	// prs := &progressReaderSeeker{RS: rs, Length: bEnd, ProgressFn: func(pos, length int64) {
-	// 	fmt.Fprintf(os.Stderr, "\r%.1f%%", float64(pos*100)/float64(length))
+	// 	fmt.Fprintf(os.Stderr, " %.1f%%\r", float64(pos*100)/float64(length))
 	// }}
+
+	// prs := newProgressReaderSeeker2(rs, bEnd, func(readBytes int64, length int64) {
+	// 	fmt.Fprintf(os.Stderr, " %.1f%%\r", (float64(readBytes)/float64(length))*100)
+	// })
 
 	return &Buffer{
 		br:     NewReaderFromReadSeeker(aheadreadseeker.New(rs, cacheReadAheadSize)),
