c44c632fe6478ea59c79d9f6030ed1401d23139b
Mattias Wadman <mattias.wadman@gmail.com>
2021-01-19 02:21:15 +0100

    wip

---
+++ pkg/format/dns/dns.go
@@ -0,0 +1,173 @@
+package dns
+
+import (
+	"fq/pkg/decode"
+	"fq/pkg/format"
+	"strings"
+)
+
+func init() {
+	format.MustRegister(&decode.Format{
+		Name:        format.DNS,
+		Description: "DNS packet",
+		DecodeFn:    dnsDecode,
+	})
+}
+
+// TODO: type consts
+// TODO: aaaa rddata
+
+var typeNames = map[uint64]string{
+	1:     "A",
+	28:    "AAAA",
+	18:    "AFSDB",
+	42:    "APL",
+	257:   "CAA",
+	60:    "CDNSKEY",
+	59:    "CDS",
+	37:    "CERT",
+	5:     "CNAME",
+	62:    "CSYNC",
+	49:    "DHCID",
+	32769: "DLV",
+	39:    "DNAME",
+	48:    "DNSKEY",
+	43:    "DS",
+	108:   "EUI48",
+	109:   "EUI64",
+	13:    "HINFO",
+	55:    "HIP",
+	45:    "IPSECKEY",
+	25:    "KEY",
+	36:    "KX",
+	29:    "LOC",
+	15:    "MX",
+	35:    "NAPTR",
+	2:     "NS",
+	47:    "NSEC",
+	50:    "NSEC3",
+	51:    "NSEC3PARAM",
+	61:    "OPENPGPKEY",
+	12:    "PTR",
+	46:    "RRSIG",
+	17:    "RP",
+	24:    "SIG",
+	53:    "SMIMEA",
+	6:     "SOA",
+	33:    "SRV",
+	44:    "SSHFP",
+	32768: "TA",
+	249:   "TKEY",
+	52:    "TLSA",
+	250:   "TSIG",
+	16:    "TXT",
+	256:   "URI",
+	63:    "ZONEMD",
+	64:    "SVCB",
+	65:    "HTTPS",
+}
+
+var rcodeNames = map[uint64]string{
+	0: "No error",
+	1: "Format error",
+	2: "Server failure",
+	3: "Name error",
+	4: "Not implemented",
+	5: "Refused",
+}
+
+func fieldDecodeLabel(d *decode.D, name string) {
+	var endPos int64
+
+	d.FieldStructFn(name, func(d *decode.D) {
+
+		var ls []string
+		d.FieldArrayFn("labels", func(d *decode.D) {
+			seenTermintor := false
+			for !seenTermintor {
+				d.FieldStructFn("label", func(d *decode.D) {
+					if d.PeekBits(2) == 0b11 {
+						d.FieldU2("is_pointer")
+						pointer := d.FieldU14("pointer")
+						if endPos == 0 {
+							endPos = d.Pos()
+						}
+						d.SeekAbs(int64(pointer * 8))
+					}
+
+					l := d.FieldU8("length")
+					if l == 0 {
+						seenTermintor = true
+						return
+					}
+					ls = append(ls, d.FieldUTF8("value", int(l)))
+				})
+
+			}
+
+		})
+		d.FieldValueStr("value", strings.Join(ls, "."), "")
+	})
+
+	if endPos != 0 {
+		d.SeekAbs(endPos)
+	}
+}
+
+func fieldDecodeRR(d *decode.D, count uint64, name string, structName string) {
+	d.FieldArrayFn(name, func(d *decode.D) {
+		for i := uint64(0); i < count; i++ {
+			d.FieldStructFn(structName, func(d *decode.D) {
+				fieldDecodeLabel(d, "name")
+				typ, _ := d.FieldStringMapFn("type", typeNames, "Unknown", d.U16)
+				d.FieldU16("class")
+				d.FieldU32("ttl")
+				// TODO: pointer?
+				rdLength := d.FieldU16("rd_length")
+
+				switch typ {
+				case 5:
+					fieldDecodeLabel(d, "cname")
+				default:
+					d.FieldUTF8("rddata", int(rdLength))
+				}
+
+			})
+		}
+	})
+}
+
+func dnsDecode(d *decode.D, in interface{}) interface{} {
+	d.FieldStructFn("header", func(d *decode.D) {
+		d.FieldU16("id")
+		d.FieldBool("query")
+		d.FieldU4("opcode")
+		d.FieldBool("authoritative_answer")
+		d.FieldBool("truncation")
+		d.FieldBool("recursion_desired")
+		d.FieldBool("recursion_available")
+		d.FieldU3("z")
+		d.FieldStringMapFn("rcode", rcodeNames, "Unknown", d.U4)
+	})
+
+	qdCount := d.FieldU16("qd_count")
+	anCount := d.FieldU16("an_count")
+	nsCount := d.FieldU16("ns_count")
+	arCount := d.FieldU16("ar_count")
+
+	d.FieldArrayFn("questions", func(d *decode.D) {
+		for i := uint64(0); i < qdCount; i++ {
+			d.FieldStructFn("question", func(d *decode.D) {
+				fieldDecodeLabel(d, "name")
+				d.FieldStringMapFn("type", typeNames, "Unknown", d.U16)
+				d.FieldU16("class")
+			})
+		}
+	})
+
+	fieldDecodeRR(d, anCount, "answers", "answer")
+	fieldDecodeRR(d, nsCount, "nameservers", "nameserver")
+	fieldDecodeRR(d, arCount, "additionals", "additional")
+
+	return nil
+}
