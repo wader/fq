23c55398d0448dc3b6bdc384d283cd60b2d9f061
Mattias Wadman <mattias.wadman@gmail.com>
2021-01-29 23:56:58 +0100

    wip

--- internal/aheadreadseeker/aheadreadseeker.go
+++ internal/aheadreadseeker/aheadreadseeker.go
@@ -17,10 +17,10 @@ type Reader struct {
 	rs      io.ReadSeeker
 	minRead int
 
-	off       int64
-	cache     []byte
-	cacheOff  int64
-	cacheUsed int
+	offset      int64
+	cache       []byte
+	cacheOffset int64
+	cacheUsed   int
 }
 
 func New(rs io.ReadSeeker, minRead int) *Reader {
@@ -32,11 +32,11 @@ func New(rs io.ReadSeeker, minRead int) *Reader {
 
 func (r *Reader) Read(p []byte) (n int, err error) {
 	for {
-		if r.off >= r.cacheOff && r.off < r.cacheOff+int64(r.cacheUsed) {
-			d := r.off - r.cacheOff
+		if r.offset >= r.cacheOffset && r.offset < r.cacheOffset+int64(r.cacheUsed) {
+			d := r.offset - r.cacheOffset
 			copyLen := min64(int64(r.cacheUsed)-d, int64(len(p)))
 			copy(p[0:copyLen], r.cache[d:d+copyLen])
-			r.off += copyLen
+			r.offset += copyLen
 
 			return int(copyLen), nil
 		}
@@ -50,7 +50,7 @@ func (r *Reader) Read(p []byte) (n int, err error) {
 		}
 
 		n, err := io.ReadFull(r.rs, r.cache[0:readBytes])
-		r.cacheOff = r.off
+		r.cacheOffset = r.offset
 		r.cacheUsed = n
 		if n == 0 || (err != io.ErrUnexpectedEOF && err == io.EOF) {
 			return 0, err
@@ -66,7 +66,7 @@ func (r *Reader) Seek(offset int64, whence int) (int64, error) {
 	case io.SeekStart:
 		absOff = offset
 	case io.SeekCurrent:
-		absOff = r.off + offset
+		absOff = r.offset + offset
 	case io.SeekEnd:
 		absOff, err = r.rs.Seek(offset, whence)
 		if err != nil {
@@ -74,8 +74,8 @@ func (r *Reader) Seek(offset int64, whence int) (int64, error) {
 		}
 	}
 
-	if absOff >= r.cacheOff && absOff < r.cacheOff+int64(r.cacheUsed) {
-		r.off = absOff
+	if absOff >= r.cacheOffset && absOff < r.cacheOffset+int64(r.cacheUsed) {
+		r.offset = absOff
 		return absOff, nil
 	}
 
@@ -83,8 +83,8 @@ func (r *Reader) Seek(offset int64, whence int) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	r.off = absOff
-	r.cacheOff = 0
+	r.offset = absOff
+	r.cacheOffset = 0
 	r.cacheUsed = 0
 
 	return absOff, nil
--- internal/progressreadseeker/progressreaderseeker.go
+++ internal/progressreadseeker/progressreaderseeker.go
@@ -13,7 +13,7 @@ import "io"
 
 const progressPrecision = 1024
 
-type progressReaderSeeker2 struct {
+type progressReaderSeeker struct {
 	rs             io.ReadSeeker
 	pos            int64
 	length         int64
@@ -22,8 +22,8 @@ type progressReaderSeeker2 struct {
 	progressFn     func(readBytes int64, length int64)
 }
 
-func newProgressReaderSeeker2(rs io.ReadSeeker, length int64, fn func(pos int64, length int64)) *progressReaderSeeker2 {
-	return &progressReaderSeeker2{
+func newProgressReaderSeeker(rs io.ReadSeeker, length int64, fn func(pos int64, length int64)) *progressReaderSeeker {
+	return &progressReaderSeeker{
 		rs:         rs,
 		length:     length,
 		partitions: make([]bool, progressPrecision+1),
@@ -31,7 +31,7 @@ func newProgressReaderSeeker2(rs io.ReadSeeker, length int64, fn func(pos int64,
 	}
 }
 
-func (prs *progressReaderSeeker2) Read(p []byte) (n int, err error) {
+func (prs *progressReaderSeeker) Read(p []byte) (n int, err error) {
 	n, err = prs.rs.Read(p)
 	newPos := prs.pos + int64(n)
 	lastPartitionsRead := prs.partitionsRead
@@ -58,34 +58,4 @@ func (prs *progressReaderSeeker2) Read(p []byte) (n int, err error) {
 	prs.pos = newPos
 
 	return n, err
-}
-
-func (prs *progressReaderSeeker2) Seek(offset int64, whence int) (int64, error) {
-	pos, err := prs.rs.Seek(offset, whence)
-	prs.pos = pos
-	return pos, err
-}
-
-type progressReaderSeeker struct {
-	RS         io.ReadSeeker
-	Length     int64
-	Pos        int64
-	MaxPos     int64
-	ProgressFn func(pos int64, length int64)
-}
-
-func (prs *progressReaderSeeker) Read(p []byte) (n int, err error) {
-	n, err = prs.RS.Read(p)
-	prs.Pos += int64(n)
-	if prs.Pos > prs.MaxPos {
-		prs.MaxPos = prs.Pos
-		prs.ProgressFn(prs.MaxPos, prs.Length)
-	}
-	return n, err
-}
-
-func (prs *progressReaderSeeker) Seek(offset int64, whence int) (int64, error) {
-	pos, err := prs.RS.Seek(offset, whence)
-	prs.Pos = pos
-	return pos, err
 }
