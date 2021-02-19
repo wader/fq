fac81645332be38516a31fdfbad4b4e25c5cedd4
Mattias Wadman <mattias.wadman@gmail.com>
2021-02-19 01:06:39 +0100

    wip

---
+++ pkg/query/query_test.go
@@ -0,0 +1,55 @@
+package query_test
+
+import (
+	"fq/pkg/query"
+	"testing"
+)
+
+func TestBuildCompletionQuery(t *testing.T) {
+	testCases := []struct {
+		input          string
+		expectedQuery  string
+		expectedType   query.CompletionType
+		expectedPrefix string
+	}{
+		{"", "", query.CompletionTypeNone, ""},
+		{`.`, `.`, query.CompletionTypeIndex, ``},
+		{`.`, `.`, query.CompletionTypeIndex, ``},
+		{`.a`, `.`, query.CompletionTypeIndex, `a`},
+		{`.a.`, `.a`, query.CompletionTypeIndex, ``},
+		{`.a.b`, `.a`, query.CompletionTypeIndex, `b`},
+		{`.a.b.`, `.a.b`, query.CompletionTypeIndex, ``},
+		{` .a.b`, `.a`, query.CompletionTypeIndex, `b`},
+		{`.a | .b`, `.a | .`, query.CompletionTypeIndex, `b`},
+		{`.a | .b.c`, `.a | .b`, query.CompletionTypeIndex, `c`},
+		{`.a[]`, ``, query.CompletionTypeNone, ``},
+		{`.a[].b`, `.a[]`, query.CompletionTypeIndex, `b`},
+		{`.a[].b.c`, `.a[].b`, query.CompletionTypeIndex, `c`},
+		{`.a["b"]`, ``, query.CompletionTypeNone, ``},
+		{`.a["b"].c`, `.a["b"]`, query.CompletionTypeIndex, `c`},
+		{`.a[1:2]`, ``, query.CompletionTypeNone, ``},
+		{`.a[1:2].c`, `.a[1:2]`, query.CompletionTypeIndex, `c`},
+
+		{`a`, ``, query.CompletionTypeFunc, `a`},
+		{`a | b`, ``, query.CompletionTypeFunc, `b`},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.input, func(t *testing.T) {
+			actualQuery, actualType, actualPrefix := query.BuildCompletionQuery(tC.input)
+			actualQueryStr := ""
+			if actualQuery != nil {
+				actualQueryStr = actualQuery.String()
+			}
+
+			if tC.expectedQuery != actualQueryStr {
+				t.Errorf("expected query %q, got query %q", tC.expectedQuery, actualQueryStr)
+			}
+			if tC.expectedType != actualType {
+				t.Errorf("expected type %s, got type %s", tC.expectedType, actualType)
+			}
+			if tC.expectedPrefix != actualPrefix {
+				t.Errorf("expected prefix %s, got prefix %q", tC.expectedPrefix, actualPrefix)
+			}
+		})
+	}
+}
