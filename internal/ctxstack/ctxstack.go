03c6ef4a06f4725602233e894f7af0f96007084e
Mattias Wadman <mattias.wadman@gmail.com>
2021-03-07 13:22:34 +0100

    wip

---
+++ internal/ctxstack/ctxstack.go
@@ -0,0 +1,48 @@
+// ctxstack manages a stack of contexts. When triggerFn returns and closeCh is not closed
+// it will cancel the top context. Stack is popped when returned cancel funcition is called.
+// Cancel functions need to be cancelled in reverse they were pushed.
+// This can be used to keep track of contexts for nested REPL:s
+package ctxstack
+
+import (
+	"context"
+)
+
+type Stack struct {
+	cancelFns []func()
+	closeCh   chan struct{}
+}
+
+func New(triggerCh func(closeCh chan struct{})) *Stack {
+	closeCh := make(chan struct{})
+	s := &Stack{closeCh: closeCh}
+
+	go func() {
+		for {
+			triggerCh(closeCh)
+			select {
+			case <-closeCh:
+			default:
+				s.cancelFns[len(s.cancelFns)-1]()
+				continue
+			}
+			break
+		}
+	}()
+
+	return s
+}
+
+func (s *Stack) Close() {
+	close(s.closeCh)
+}
+
+func (s *Stack) Push(parent context.Context) (context.Context, func()) {
+	stackCtx, stackCtxCancel := context.WithCancel(parent)
+	s.cancelFns = append(s.cancelFns, stackCtxCancel)
+
+	return stackCtx, func() {
+		s.cancelFns = s.cancelFns[0 : len(s.cancelFns)-1]
+		stackCtxCancel()
+	}
+}
