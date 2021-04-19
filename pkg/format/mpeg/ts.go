a2c157759da6462abbf796c3a0c392b218b27e4a
Mattias Wadman <mattias.wadman@gmail.com>
2021-04-19 15:53:52 +0200

    wip

---
+++ pkg/format/mpeg/ts.go
@@ -0,0 +1,30 @@
+package mpeg
+
+import (
+	"fq/pkg/decode"
+	"fq/pkg/format"
+)
+
+func init() {
+	format.MustRegister(&decode.Format{
+		Name:        format.MPEG_TS,
+		Description: "MPEG Transport Stream",
+		Groups:      []string{format.PROBE},
+		DecodeFn:    tsDecode,
+	})
+}
+
+// TODO: ts_packet
+
+func tsDecode(d *decode.D, in interface{}) interface{} {
+	d.FieldValidateUFn("sync", 0x47, d.U8)
+	d.FieldBool("transport_error_indicator")
+	d.FieldBool("payload_unit_start")
+	d.FieldBool("transport_priority")
+	d.FieldU13("pid")
+	d.FieldU2("transport_scrambling_control")
+	d.FieldU2("adaptation_field_control")
+	d.FieldU4("continuity_counter")
+
+	return nil
+}
