e5c635f269fa6c9cc1e2f0cf2526b27895585328
Mattias Wadman <mattias.wadman@gmail.com>
2021-08-20 11:25:31 +0200

    icc_profile: Clamp alignment to end instead of check last tag (might not be last in buffer)

--- format/icc/icc_profile.go
+++ format/icc/icc_profile.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/wader/fq/format"
 	"github.com/wader/fq/format/registry"
+	"github.com/wader/fq/internal/num"
 	"github.com/wader/fq/pkg/decode"
 )
 
@@ -89,8 +90,6 @@ func iccProfileDecode(d *decode.D, in interface{}) interface{} {
 	   100..127 28 bytes reserved for future expansion - must be set to zeros
 	*/
 
-	startByteOffset := d.Pos() / 8
-
 	// TODO: PokeU32()?
 	size := d.U32()
 	d.SeekRel(-4 * 8)
@@ -149,10 +148,11 @@ func iccProfileDecode(d *decode.D, in interface{}) interface{} {
 
 						// "All tag data is required to start on a 4-byte boundary (relative to the start of the profile data stream)"
 						// we can't add this at the start of the element as we don't know how big the previous element in the stream
-						// was. instead add alignment after if offset+size does not align and we're not at the last tag
-						paddingBytes := (4 - (int64(offset)-startByteOffset+int64(size))%4) % 4
-						if i < tagCount-1 && paddingBytes != 0 {
-							paddingStart := int64(offset) + int64(size)
+						// was. instead add alignment after if offset+size does not align and to be sure clamp it if outside buffer.
+						paddingStart := int64(offset) + int64(size)
+						paddingBytes := (4 - (int64(offset)+int64(size))%4) % 4
+						paddingBytes = num.MinInt64(paddingBytes, d.Len()-(paddingStart+paddingBytes))
+						if paddingBytes != 0 {
 							d.FieldBitBufRange("alignment", paddingStart*8, paddingBytes*8)
 						}
 					})
