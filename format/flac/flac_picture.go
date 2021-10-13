509b8f8c5010307a5518e85dcc99f2135e1d5c15
Mattias Wadman <mattias.wadman@gmail.com>
2021-10-13 21:34:14 +0200

    flac_picture: Add picture_type names

--- format/flac/flac_picture.go
+++ format/flac/flac_picture.go
@@ -8,6 +8,30 @@ import (
 
 var images []*decode.Format
 
+var pictureTypeNames = map[uint64]string{
+	0:  "Other",
+	1:  "32x32 pixels 'file icon' (PNG only)",
+	2:  "Other file icon",
+	3:  "Cover (front)",
+	4:  "Cover (back)",
+	5:  "Leaflet page",
+	6:  "Media (e.g. label side of CD)",
+	7:  "Lead artist/lead performer/soloist",
+	8:  "Artist/performer",
+	9:  "Conductor",
+	10: "Band/Orchestra",
+	11: "Composer",
+	12: "Lyricist/text writer",
+	13: "Recording Location",
+	14: "During recording",
+	15: "During performance",
+	16: "Movie/video screen capture",
+	17: "A bright colored fish",
+	18: "Illustration",
+	19: "Band/artist logotype",
+	20: "Publisher/Studio logotype",
+}
+
 func init() {
 	registry.MustRegister(&decode.Format{
 		Name:        format.FLAC_PICTURE,
@@ -24,7 +48,7 @@ func pictureDecode(d *decode.D, in interface{}) interface{} {
 		l := d.FieldU32(name + "_length")
 		return d.FieldUTF8(name, int(l))
 	}
-	d.FieldU32("picture_type")
+	d.FieldStringMapFn("picture_type", pictureTypeNames, "Unknown", d.U32, decode.NumberDecimal)
 	lenStr("mime")
 	lenStr("description")
 	d.FieldU32("width")
