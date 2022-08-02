d8b891c01946c57a671ec43185a7dea043ed419c
Pavel Safonov <p.safonov@postgrespro.ru>
2022-08-02 17:20:50 +0300

    PostgreSQL: pgproee11 pg_control impl

--- format/postgres/pg_control.go
+++ format/postgres/pg_control.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"github.com/wader/fq/format"
+	"github.com/wader/fq/format/postgres/flavours/pgproee11"
 	"github.com/wader/fq/format/postgres/flavours/pgproee14"
 	"github.com/wader/fq/format/postgres/flavours/postgres11"
 	"github.com/wader/fq/format/postgres/flavours/postgres14"
@@ -42,6 +43,8 @@ func decodePgControl(d *decode.D, in any) any {
 		return postgres11.DecodePgControl(d, in)
 	case PG_FLAVOUR_POSTGRES14, PG_FLAVOUR_POSTGRES:
 		return postgres14.DecodePgControl(d, in)
+	case PG_FLAVOUR_PGPROEE11:
+		return pgproee11.DecodePgControl(d, in)
 	case PG_FLAVOUR_PGPROEE14:
 		return pgproee14.DecodePgControl(d, in)
 	default:
