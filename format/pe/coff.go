1c72ff285b8c0dc7a577516862fb32309bc0ce67
Mattias Wadman <mattias.wadman@gmail.com>
2023-09-17 11:52:18 +0200

    wip

--- format/pe/coff.go
+++ format/pe/coff.go
@@ -9,6 +9,7 @@ package pe
 
 import (
 	"encoding/binary"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -487,7 +488,7 @@ func peCoffStubDecode(d *decode.D) any {
 			d.FieldStruct("section", func(d *decode.D) {
 				name := d.FieldUTF8("name", 8, stringTableMapper)                     // An 8-byte, null-padded UTF-8 encoded string. If the string is exactly 8 characters long, there is no terminating null. For longer names, this field contains a slash (/) that is followed by an ASCII representation of a decimal number that is an offset into the string table. Executable images do not use a string table and do not support section names longer than 8 characters. Long names in object files are truncated if they are emitted to an executable file.
 				d.FieldU32("virtual_size")                                            // The total size of the section when loaded into memory. If this value is greater than SizeOfRawData, the section is zero-padded. This field is valid only for executable images and should be set to zero for object files.
-				d.FieldU32("virtual_address", scalar.UintHex)                         // For executable images, the address of the first byte of the section relative to the image base when the section is loaded into memory. For object files, this field is the address of the first byte before relocation is applied; for simplicity, compilers should set this to zero. Otherwise, it is an arbitrary value that is subtracted from offsets during relocation.
+				virtualAddress := d.FieldU32("virtual_address", scalar.UintHex)       // For executable images, the address of the first byte of the section relative to the image base when the section is loaded into memory. For object files, this field is the address of the first byte before relocation is applied; for simplicity, compilers should set this to zero. Otherwise, it is an arbitrary value that is subtracted from offsets during relocation.
 				sizeOfRawData := d.FieldU32("size_of_raw_data")                       // The size of the section (for object files) or the size of the initialized data on disk (for image files). For executable images, this must be a multiple of FileAlignment from the optional header. If this is less than VirtualSize, the remainder of the section is zero-filled. Because the SizeOfRawData field is rounded but the VirtualSize field is not, it is possible for SizeOfRawData to be greater than VirtualSize as well. When a section contains only uninitialized data, this field should be zero.
 				pointerToRawData := d.FieldU32("pointer_to_raw_data", scalar.UintHex) // The file pointer to the first page of the section within the COFF file. For executable images, this must be a multiple of FileAlignment from the optional header. For object files, the value should be aligned on a 4-byte boundary for best performance. When a section contains only uninitialized data, this field should be zero.
 				d.FieldU32("pointer_to_relocations", scalar.UintHex)                  // The file pointer to the beginning of relocation entries for the section. This is set to zero for executable images or if there are no relocations.
@@ -611,6 +612,8 @@ func peCoffStubDecode(d *decode.D) any {
 									}
 								})
 
+								_ = virtualAddress
+
 								d.FieldArray("dlls", func(d *decode.D) {
 
 									for i := 0; i < count; i++ {
@@ -627,6 +630,30 @@ func peCoffStubDecode(d *decode.D) any {
 											})
 											d.FieldU("lookup_table1_end", addrSize)
 										})
+									}
+
+								})
+
+								d.FieldArray("hint_names", func(d *decode.D) {
+
+									for i := 0; true; i++ {
+
+										log.Printf("i: %#+v\n", i)
+
+										d.FieldStruct("hint_name", func(d *decode.D) {
+
+											d.FieldU16("hint")
+											d.FieldUTF8Null("name")
+
+											// if d.AlignBits(16) != 0 {
+											// 	d.FieldU8("align_pad")
+											// }
+
+										})
+
+										if i > 800 {
+											break
+										}
 									}
 
 								})
