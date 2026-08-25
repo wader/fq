343918b5069aea62f278a36bb612529bb77f5d47
Chris (ChrisJr404) <11917633+ChrisJr404@users.noreply.github.com>
2026-08-25 16:13:49 -0400

    bmp: Add decoder

--- format/format.go
+++ format/format.go
@@ -81,6 +81,7 @@ var (
 	Bitcoin_Block       = &decode.Group{Name: "bitcoin_block"}
 	Bitcoin_Script      = &decode.Group{Name: "bitcoin_script"}
 	Bitcoin_Transaction = &decode.Group{Name: "bitcoin_transaction"}
+	BMP                 = &decode.Group{Name: "bmp"}
 	Bplist              = &decode.Group{Name: "bplist"}
 	BSD_Loopback_Frame  = &decode.Group{Name: "bsd_loopback_frame"}
 	BSON                = &decode.Group{Name: "bson"}
